bc017cdc762bc72df96a7657229ca2dee7aa3383
Juan Bermudez <juan.bermudez@linecorp.com>
2017-03-01 07:49:00 +0900

    Remove test

--- src/test/java/com/yoanalizer/DemoApplicationTests.java
+++ src/test/java/com/yoanalizer/DemoApplicationTests.java
@@ -1,10 +1,5 @@
 package com.yoanalizer;
 
-import java.io.IOException;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,14 +7,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
-
-	@Test
-	public void contextLoads() throws IOException {
-		String key = "AIzaSyA2Uel2GJUA2-QI_ZSGooigcULMkp_3gvo";
-		String videoId = "9PhqF59ls08";
-		String url = "https://www.googleapis.com/youtube/v3/videos?id="+ videoId + "&fields=items(snippet(title))&part=snippet&key="+ key;
-		Document doc = Jsoup.connect(url).timeout(10 * 1000).get();
-		int a  = 2;
-	}
 
 }
