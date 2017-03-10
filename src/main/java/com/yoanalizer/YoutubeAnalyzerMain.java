c7e69986a490885c95c2323c718a55af51bb74fd
Juan Bermudez <juan.bermudez@linecorp.com>
2017-03-10 21:49:51 +0900

    Disable cross origin

--- src/main/java/com/yoanalizer/YoutubeAnalyzerMain.java
+++ src/main/java/com/yoanalizer/YoutubeAnalyzerMain.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class YoutubeAnalyzerMain {
@@ -15,6 +18,16 @@ public class YoutubeAnalyzerMain {
         executor.setMaxPoolSize(10);
         executor.setQueueCapacity(25);
         return executor;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/").allowedOrigins("http://localhost:8080");
+            }
+        };
     }
 
     public static void main(String[] args) {
